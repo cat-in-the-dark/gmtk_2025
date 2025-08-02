3a37207649b76efec05d9bf8919bba62c481d249
Dmitry Shatov <d.shatty@gmail.com>
2025-08-02 22:13:18 +0600

    jump: terminate, coyote, input buffering

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,43 +1,81 @@
 extends CharacterBody2D
 class_name Player
 
-const SPEED = 96.0
-const JUMP_VELOCITY = -300.0
-const CIRCLE_LEN = PI * 16
+const ACCEL = 180
+const MAX_SPEED_X = 180
+const MAX_SPEED_FALL = 380
+const GRAVITY = 700
+const JUMP_GRAVITY = 500
+const JUMP_FORCE = 250
 
 var started = false
+var now = 0
+var on_floor_timestamp = 0
+var jump_pressed_timestamp = 0
 
 @onready var sprite = $Sprite2D
 
 func _physics_process(delta):
+	now += delta
 	if not started:
 		if Input.is_action_just_pressed("ui_accept"):
 			started = true
 		return
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var y_input = 0
+	if Input.is_action_just_pressed("player_jump"):
+		jump_pressed_timestamp = now
+	if now - jump_pressed_timestamp < 0.15:
+		y_input = -1
+	if Input.is_action_just_released("player_jump"):
+		y_input = 1
+		jump_pressed_timestamp = 0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	# var direction = Input.get_axis("ui_left", "ui_right")
-	var direction = 1 # always moving
+	velocity.x += ACCEL * delta
+	velocity.x = clamp(velocity.x, -MAX_SPEED_X, MAX_SPEED_X)
 
-	if direction:
-		velocity.x = direction * SPEED
+	var was_on_floor = is_on_floor()
+
+	if (velocity.y < 0):
+		velocity.y += JUMP_GRAVITY * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y += GRAVITY * delta
+	if velocity.y > MAX_SPEED_FALL:
+		velocity.y = MAX_SPEED_FALL
+	if (is_on_floor() or now - on_floor_timestamp < 0.15) and velocity.y > 0:
+		if y_input < 0:
+			on_floor_timestamp = 0
+			# $SFX/RandomJump.play()
+			velocity.y = -JUMP_FORCE
+	else:
+		# animation.play("jump_up")
+		if y_input > 0 and velocity.y < 0:
+			velocity.y = 0
 
-	# Animate sprite
-	if velocity.x > 0.01 or velocity.x < -0.01:
-		sprite.rotate(-1 * velocity.x / 16.0)
+	if abs(velocity.x) < 3:
+		velocity.x = 0
 
 	move_and_slide()
+
+	if !was_on_floor and is_on_floor():
+		# $SFX/RandomFallOnGround.play()
+		pass
+
+	if is_on_floor():
+		on_floor_timestamp = now
+		if abs(velocity.y) < 5:
+			velocity.y = 0
+		if velocity.x != 0:
+			pass
+			#if !$SFX/RandomStep.playing:
+			#	$SFX/RandomStep.play()
+#
+	## Animate sprite
+	var vx = get_position_delta().x
+	#if vx > 1 or vx < -1:
+	sprite.rotate(vx / 16)
+#
+	#move_and_slide()
 
 
 func collect_note():

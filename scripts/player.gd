12ef72d5dff051c1415192bb1a73fdb177569669
Dmitry Shatov <d.shatty@gmail.com>
2025-08-02 22:58:00 +0600

    handle release jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -22,6 +22,8 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+		velocity.y = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

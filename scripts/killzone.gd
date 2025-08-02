60729bed1eaec2ac7f5cef62ff8db06570a07a6d
Ilya Siganov <ilya.siganov@gmail.com>
2025-08-02 22:58:15 +0600

    add instructions and design room1

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+func restart_game():
+	get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
 
 func _on_body_entered(body):
 	if body is Player:
 		print("GAME OVER")
 		# TODO: gameover or restart of the room?
-		get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
+		call_deferred("restart_game")

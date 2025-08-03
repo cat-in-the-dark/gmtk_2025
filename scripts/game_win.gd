d0604d1c5f09a76fc7c0c55cdac9ee1171d2a230
Ilya Siganov <ilya.blan4@gmail.com>
2025-08-03 17:55:46 +0600

    add game win

---
+++ scripts/game_win.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func _ready():
+	$restarts.text = "LOOPS USED: %d" % Globals.n_restarts 
+
+func restart_game():
+	get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		call_deferred("restart_game")

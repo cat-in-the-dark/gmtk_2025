5fc95f189f632e6488d77cacd97636f71c5d6ebd
Dmitry Shatov <d.shatty@gmail.com>
2025-08-03 13:39:42 +0600

    bgm player (draft)

---
+++ scripts/bgm_player.gd
@@ -0,0 +1,30 @@
+extends AudioStreamPlayer
+class_name BgmPlayer
+
+var playing_track: int = 0
+var target_track: int = 0
+var clip_count: int = 0;
+
+func _ready() -> void:
+	var s = stream as AudioStreamInteractive
+	clip_count = s.clip_count
+
+
+func _decide_track() -> int:
+	return target_track;
+
+func _process(delta: float) -> void:
+	var next_track := _decide_track()
+	if next_track != playing_track:
+		playing_track = next_track
+		var playback := get_stream_playback() as AudioStreamPlaybackInteractive
+		playback.switch_to_clip(playing_track)
+
+
+func _on_room_note_collected(n_notes: int, n_collected_notes: int) -> void:
+	print('_on_room_note_collected:', n_notes, n_collected_notes)
+	n_collected_notes = min(n_collected_notes, n_notes)
+	@warning_ignore("integer_division")
+	target_track = n_collected_notes * clip_count / n_notes
+	print('target_track:', target_track )
+	pass # Replace with function body.
